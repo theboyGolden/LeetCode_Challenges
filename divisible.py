0c182cf0c1c44339abd94f3b435a8d4f563a2e7c
theboyGolden <preciousstick15@gmail.com>
2024-01-14 00:37:15 +0000

    Divisible problem

--- divisible.py
+++ divisible.py
@@ -1,5 +1,7 @@
 ## Write a method that displays all the numbers from 10 to 1000 that are divisible by 5 and 6. 
 
+
+## Solution
 def display_numbers_divisible_by_5_and_6():
     # Iterate through numbers from 10 to 1000
     for num in range(10, 1001):

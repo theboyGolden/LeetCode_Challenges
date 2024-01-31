210e9222e087283d29eedaa2d5042a111902c8ca
theboyGolden <preciousstick15@gmail.com>
2024-01-31 23:18:11 +0000

    .

--- PowerofFour.py
+++ PowerofFour.py
@@ -26,3 +26,5 @@
 
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
+        # Check if n is positive and a power of two
+        return n > 0 and (n & (n - 1)) == 0 and (n - 1) % 3 == 0

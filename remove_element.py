26e026733116756137dc5e08466d8fa7cd6cea02
theboyGolden <preciousstick15@gmail.com>
2024-02-01 23:04:37 +0000

    .

--- remove_element.py
+++ remove_element.py
@@ -53,3 +53,6 @@
 
 class Solution {
     public int removeElement(int[] nums, int val) {
+        int k = 0; // variable to track the number of elements not equal to val
+
+        for (int i = 0; i < nums.length; i++) {

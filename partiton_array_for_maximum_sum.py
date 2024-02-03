6c07da7a9e2a8b65a7e87a784ddbe645ee212f7e
theboyGolden <preciousstick15@gmail.com>
2024-02-03 09:50:36 +0000

    .

--- partiton_array_for_maximum_sum.py
+++ partiton_array_for_maximum_sum.py
@@ -38,3 +38,8 @@ def maxSumAfterPartitioning(arr, k):
 
     for i in range(n):
         max_val = float('-inf')
+        for j in range(1, min(k, i + 1) + 1):
+            max_val = max(max_val, arr[i - j + 1])
+            dp[i] = max(dp[i], (dp[i - j] if i - j >= 0 else 0) + max_val * j)
+
+    return dp[-1]

efc056c54d705066740e4b24064dacb5ed1e6c80
theboyGolden <preciousstick15@gmail.com>
2024-02-09 21:44:18 +0000

    .

--- Largest Divisible Subset.py
+++ Largest Divisible Subset.py
@@ -1,7 +1,5 @@
 # 368. Largest Divisible Subset
-# Medium
-# Topics
-# Companies
+
 # Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
 
 # answer[i] % answer[j] == 0, or
@@ -26,3 +24,12 @@
 # 1 <= nums.length <= 1000
 # 1 <= nums[i] <= 2 * 109
 # All the integers in nums are unique.
+
+
+
+def largestDivisibleSubset(nums):
+    if not nums:
+        return []
+    
+    nums.sort()
+    

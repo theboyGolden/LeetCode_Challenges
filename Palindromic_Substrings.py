c0167cfce5abfafa9bda5ca0fcde4953f4302837
theboyGolden <preciousstick15@gmail.com>
2024-02-10 20:55:08 +0000

    .

--- Palindromic_Substrings.py
+++ Palindromic_Substrings.py
@@ -27,3 +27,13 @@
 
 # 1 <= s.length <= 1000
 # s consists of lowercase English letters.
+
+
+def countSubstrings(s: str) -> int:
+    def expandAroundCenter(left, right):
+        count = 0
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
+        return count
